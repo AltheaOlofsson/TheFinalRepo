69192c6051f2ad4fc38ff3fbecba410bc3c1d5b5
BiggansFot <81302543+BiggansFot@users.noreply.github.com>
2023-10-30 12:01:13 +0100

    update monster

--- Monsters.java
+++ Monsters.java
@@ -1,3 +1,7 @@
 public class Monsters {
-    
+    private int hitPoints;
+    private int attack;
+    private double hitRate;
+    private int speed;
+    private Level[] level = {new Level(1), new Level(2), new Level(3)};
 }
