8a7a0ecbd05c4cc1fa57ce93daa37599a747c476
Freddiefreeman <fredrik.ekebro@live.se>
2023-10-30 11:10:41 +0100

    Player added a start

---
+++ Player.java
@@ -0,0 +1,12 @@
+
+
+class Player
+{
+    private String name;
+    private int hitPoints;
+    private int strength;
+    private int defence;
+    private int agility;
+    private int moneyPouch;
+}
+
