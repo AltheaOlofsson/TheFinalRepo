8aaae8d0a5b1a6e9670a3ec016b30cea8cdcbf7e
BiggansFot <bigfoot.ershag@hotmail.com>
2023-11-02 11:17:42 +0100

    monster updated

--- Monsters.java
+++ Monsters.java
@@ -1,7 +1,42 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Monsters {
+    private String name;
     private int hitPoints;
-    private int attack;
-    private double hitRate;
+    private int minAttack;
+    private int maxAttack;
+    private int level;
     private int speed;
-    private Level[] level = {new Level(1), new Level(2), new Level(3)};
+    
+    Monsters(String name, int hitPoints, int minAttack, int maxAttack, int level, int speed) {
+        this.name = name;
+        this.hitPoints = hitPoints;
+        this.minAttack = minAttack;
+        this.maxAttack = maxAttack;
+        this.level = level;
+        this.speed = speed;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public int getLevel() {
+        return level;
+    }
+    public int attack() {
+        int attackDmg = ThreadLocalRandom.current().nextInt(minAttack, maxAttack + 1);
+        return attackDmg;
+    }
+/* public static void main(String[] args) {
+    Monsters ghoul = new Monsters("Ghoul", 2, 4, 8, 1, 3);
+    int damageDone = ghoul.attack();
+    System.out.println(damageDone); 
+} */
 }
+
