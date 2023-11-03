59dcf9a9f1ae9db67e442a723abdb1857481e6b6
AltheaOlofsson <althea.olofsson@iths.se>
2023-11-03 17:50:37 +0100

    dolde if occurance

--- Occurence.java
+++ Occurence.java
@@ -17,23 +17,23 @@ public void occurance1(Player p) {
     System.out.println("Apples: " + p.getApple());
 }
 
-public void occurance2(Player p) {
-    System.out.println("You meet a traveler on the road. Do you stop and chat? (Y/N)");
-    //återkommer till denna
-    //if(){
-    System.out.println("He offers to trade his dagger for a Golden Apple. Do you accept? (Y/N)");
-    //    if(input == y){
-        // if (p.getApples() != 0){
-            System.out.println("You gain a new dagger.");
-            p.addAttack(3);
-            p.addSpeed(2);
-            p.decreseApple(1);
-            System.out.println("Attack: + 3");
-            System.out.println("Speed: + 2");
-            System.out.println("You give up one Apple.");
-            System.out.println("Apple: - 1");
-      //  }else {} else{}}
-    }
+// public void occurance2(Player p) {
+//     System.out.println("You meet a traveler on the road. Do you stop and chat? (Y/N)");
+
+//     //if(){
+//     System.out.println("He offers to trade his dagger for a Golden Apple. Do you accept? (Y/N)");
+//     //    if(input == y){
+//         // if (p.getApples() != 0){
+//             System.out.println("You gain a new dagger.");
+//             p.addAttack(3);
+//             p.addSpeed(2);
+//             p.decreseApple(1);
+//             System.out.println("Attack: + 3");
+//             System.out.println("Speed: + 2");
+//             System.out.println("You give up one Apple.");
+//             System.out.println("Apple: - 1");
+//       //  }else {} else{}}
+//     }
 
     public void occurance3(Player p) {
         System.out.println("You come to an Inn by the road. You decide to take a rest.");
@@ -45,7 +45,7 @@ public void occurance2(Player p) {
         System.out.println("Speed: - 1");
     }
 
-    
+
 
 
 
