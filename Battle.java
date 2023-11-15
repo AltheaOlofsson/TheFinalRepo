8e4cbbc8228fa0f9dab8c1faf7434b62e658944d
BiggansFot <bigfoot.ershag@hotmail.com>
2023-11-15 11:26:57 +0100

    update

--- Battle.java
+++ Battle.java
@@ -7,17 +7,17 @@ public class Battle {
     Player player;
     Scanner scanner = new Scanner(System.in);
 
-    Monster slime = new Monster("Slime", 20, 2, 2, 1, 2);
-    Monster wolf = new Monster("Wolf", 25, 2, 4, 2, 15);
-    Monster goblin = new Monster("Goblin", 15, 4, 10, 2, 10);
-    Monster orc = new Monster("Orc", 20, 5, 15, 3, 10);
-    Monster direWolf = new Monster("Dire wolf", 35, 6, 12, 3, 25);
-    Monster elf = new Monster("Elf", 20, 8, 10, 3, 30);
-    Monster troll = new Monster("Troll", 55, 2, 15, 4, 8);
-    Monster ogre = new Monster("Ogre", 45, 10, 15, 4, 25);
-    Monster wurm = new Monster("Wurm", 35, 12, 14, 4, 20);
-    Monster vampire = new Monster("Vampire", 30, 8, 12, 5, 350);
-    Monster werewolf = new Monster("Werewolf", 50, 10, 20, 5, 55);
+    Monster slime = new Monster("Slime", 20, 2, 2, 1, 2, 10);
+    Monster wolf = new Monster("Wolf", 25, 2, 4, 2, 15, 7);
+    Monster goblin = new Monster("Goblin", 15, 4, 10, 2, 10, 10);
+    Monster orc = new Monster("Orc", 20, 5, 15, 3, 10, 8);
+    Monster direWolf = new Monster("Dire wolf", 35, 6, 12, 3, 25, 9);
+    Monster elf = new Monster("Elf", 20, 8, 10, 3, 30, 10);
+    Monster troll = new Monster("Troll", 55, 2, 15, 4, 8, 6);
+    Monster ogre = new Monster("Ogre", 45, 10, 15, 4, 25, 8);
+    Monster wurm = new Monster("Wurm", 35, 12, 14, 4, 20, 7);
+    Monster vampire = new Monster("Vampire", 30, 8, 12, 5, 350, 10);
+    Monster werewolf = new Monster("Werewolf", 50, 10, 20, 5, 55, 10);
 
     Monster[] monsterEncounters = {wolf,goblin,orc,direWolf,elf,troll,ogre,wurm,vampire,werewolf};
 
@@ -51,20 +51,34 @@ public class Battle {
                         System.out.println("What do you want to do? \n[1] Attack the thing! \n[2] Display stats.");
                         int attackChoice = scanner.nextInt();
                         System.out.println(attackChoice);
-                        if (attackChoice == 1) {player.attack(currentMonster);}
-                        System.out.println(currentMonster.getName() + ": " + currentMonster.getHitPoints());
+                        if (attackChoice == 1) {
+                            player.attack(currentMonster);
+                            System.out.println(currentMonster.getName() + ": " + currentMonster.getHitPoints());
+                        } else if (attackChoice == 2) {
+                            player.displayPlayerStats();
+                        } else {System.out.println("Incorrect input.");}
+                        
                         currentMonster.attack(player, currentMonster);
                         System.out.println("player:" + player.currentHp);
-                        if (attackChoice == 2) {player.displayPlayerStats();}
+                        
                     } else {
                         currentMonster.attack(player, currentMonster);
                         System.out.println("player:" + player.currentHp);
                         System.out.println("What do you want to do? \n[1] Attack the thing! \n[2] Display stats.");
                         int attackChoice = scanner.nextInt();
                         System.out.println(attackChoice);
-                        if (attackChoice == 1) {player.attack(currentMonster);}
-                        System.out.println(currentMonster.getName() + ": " + currentMonster.getHitPoints());
-                        if (attackChoice == 2) {player.displayPlayerStats();}
+                        while (attackChoice != 1) {
+                            if (attackChoice == 2) {
+                                player.displayPlayerStats();
+                            } else {System.out.println("Incorrect input.");}
+                            if (attackChoice == 1) {
+                                player.attack(currentMonster);
+                                System.out.println(currentMonster.getName() + ": " + currentMonster.getHitPoints());
+                                break;
+                            }                                                       
+                        } 
+                        
+                        
                     }
                 }
             }
