ebc626ec6fe446f57db103ed1fa4cdd37e7ac5f7
Jimmy <jimmyyiyeong@iths.se>
2023-11-19 18:08:52 +0100

    tavern event

--- EventController.java
+++ EventController.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
     public class EventController {
         static Random randomgen = new Random();
 
-        public Event[] eventEncounters = {new RootEvent(), new TravelerEvent(), new WishingStarEvent(), new FairyEvent(), new LulEvent(), new AbandonedMinesEvent()};
+        public Event[] eventEncounters = {new RootEvent(), new TravelerEvent(), new WishingStarEvent(), new FairyEvent(), new LulEvent(), new AbandonedMinesEvent(), new TavernEvent()};
 
         ArrayList<Event> eventList = new ArrayList<>(Arrays.asList(eventEncounters)); 
 
@@ -13,9 +13,10 @@ import java.util.stream.Collectors;
             List<Event> filteredEventList = eventList.stream()
             .filter(e -> (e.eventLevel <= player.getLevel()))
             .collect(Collectors.toList());
-            
+
             Event selectedEvent = filteredEventList.get(randomgen.nextInt(filteredEventList.size()));
             eventList.remove(selectedEvent);
+
             return selectedEvent;
         }
     
