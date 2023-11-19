b7b0b7060721a809dc141043860db61c625de322
Jimmy <jimmyyiyeong@iths.se>
2023-11-19 17:46:28 +0100

    text formating

--- AbandonedMinesEvent.java
+++ AbandonedMinesEvent.java
@@ -11,13 +11,15 @@ public class AbandonedMinesEvent extends Event {
 public void execute(Player player, Scanner scanner) {
 
     System.out.println("While foraging through the forest a glint of sunlight reflecting from an unusual object catches your attention. " + 
-    "Intrigued, you decide to investigate and\ndiscover an old, weathered compass half-buried in the ground. " + 
-    "With a newfound sense of direction, you follow the needle and end up at an unexpected swamp.\n" +
-    "You begin to trudge through the challenging terrain, mud grabbing at every step. " + 
-    "The swamp has an eerie stillness over it and the heavy fog is limiting your vision.\n" + 
-    "At the heart of the swamp a silhouette emerges from the mist, reavealing the entrance to an abandoned mine. " +
-    "Glancing at the compass, you notice\nit spinning uncontrollably. " + 
-    "The mystery has piqued your interest, you discard the compass and venture into the depths of the mine.");
+    "\nIntrigued, you decide to investigate anddiscover an old, weathered compass half-buried in the ground. " + 
+    "\nWith a newfound sense of direction, you follow the needle and end up at an unexpected swamp." +
+    "\nYou begin to trudge through the challenging terrain, mud grabbing at every step. " + 
+    "\nThe swamp has an eerie stillness over it and the heavy fog is limiting your vision." + 
+    "\nAt the heart of the swamp a silhouette emerges from the mist, reavealing the entrance to an abandoned mine. " + 
+    "\nGlancing at the compass, you notice it spinning uncontrollably. " + 
+    "\nThe mystery has piqued your interest, you discard the compass and venture into the depths of the mine.");
+
+    
 
 }
 
