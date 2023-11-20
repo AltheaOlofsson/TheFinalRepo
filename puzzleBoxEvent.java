b047c7b620ce8ad0c42f5b458c99fe1c48325f1d
Jimmy <jimmyyiyeong@iths.se>
2023-11-21 00:18:16 +0100

    Done

--- puzzleBoxEvent.java
+++ puzzleBoxEvent.java
@@ -15,5 +15,73 @@ public class puzzleBoxEvent extends Event {
     "\nHe presents it to you, adding 'I've had this in my posession for as long as I can remember. It's secrets elude me.." + 
     "\nPerhaps a fresh perpective is what it needs. Take it, and if your crack it's mystery, consider it your reward.");
   
+        String correctAnswer = "river";
+        int attempts = 0;
+
+        System.out.println("\nRiddle: What runs but never walks, murmurs but never talks, has a bed but never and sleeps and has a mouth but never eats?");
+
+        while (attempts < 10) {
+            // System.out.println("\nRiddle: What runs but never walks, murmurs but never talks, has a bed but never and sleeps and has a mouth but never eats?");
+            String userGuess = scanner.nextLine().toLowerCase();
+
+            if (checkGuess(userGuess, correctAnswer)) {
+                System.out.println("\nCongratulations! You've successfully solved the riddle and obtained a locket with an old picture of a unknow lady from the box." + 
+                "\nIt turns out to be the merchant's grandmother, and he offers to trade it for anything in his shop. Since it's worthless to you, you accept." + 
+                "\n\nYou pick out a few items to aid you on your journey.\nHP: + 10\nAttack: + 2\nSpeed: + 3");
+                player.addMaxHp(10);
+                player.addCurrenHp(10);
+                player.addAttack(2);
+                player.addSpeed(3);
+                player.addExperience(50);
+                break;
+                } else {
+                    System.out.println("Nothing happens. Try again.");
+                    attempts++;
+                    }   
+            if (attempts == 5) {
+                System.out.println("HINT: It's a five letter word.");
+            } else if (attempts == 8) {
+                System.out.println("HINT2: It also contains water.");
+                }
+            
+        }
+            if (attempts >= 10) {
+            System.out.println("Unfortunately, you give up and were not able to solve the riddle and handed back the box to the merchant. Disappointing.");
+            player.addExperience(70);
+            }
     }
+    
+    private static boolean checkGuess(String userGuess, String correctAnswer) {
+        return userGuess.equals(correctAnswer);
+    }
+    
 }
+
+    
+    
+    //    String correctAnswer = "key";
+    //    int attempts = 0;
+       
+    //     System.out.println("I am a three-letter word. What am I?");
+    //     String userGuess = scanner.nextLine().toLowerCase();
+
+    //     while(!checkGuess(scanner.nextLine().toLowerCase(), correctAnswer) && attempts < 10) {
+
+    //     System.out.println("I am a three-letter word. What am I?");
+    //     String userGuess = scanner.nextLine().toLowerCase();
+
+    //         System.out.println("Nothing happens. Try again.");
+    //         attempts++;
+
+    //             if (attempts < 10) {
+    //                 System.out.println("Congratulations, you solved the puzzle and obtained something amazing!");
+    //                     // add stats
+    //             } else {
+    //                 System.out.println("Unfortunately you were not able to solve the puzzle and handed it back to the merchant. Disappointing.");
+    //                 }  
+    //     }
+    //     private static boolean checkGuess(String userGuess, String correctAnswer) {
+    //         return userGuess.equals("key")
+    //     }
+
+
