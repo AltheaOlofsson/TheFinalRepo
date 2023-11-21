362f909cc404b366e45ab130bf14d8b548c182eb
Jimmyyiyeong <jimmy.yiyeong@iths.se>
2023-11-21 23:33:09 +0100

    save

---
+++ arenaEvent.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class arenaEvent extends Event {
+    
+arenaEvent() {
+    super();
+    this.eventLevel = 7;
+    }
+
+@Override
+public void execute(Player player, Scanner scanner) {
+
+
+}
+
+
+}
+
