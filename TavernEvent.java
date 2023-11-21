a572efced37a87bedf6a0e267b76381993a86653
AltheaOlofsson <althea.olofsson@iths.se>
2023-11-21 14:23:47 +0100

    added buff printout

--- TavernEvent.java
+++ TavernEvent.java
@@ -23,13 +23,16 @@ public class TavernEvent extends Event {
         if(prize == 0){
             System.out.println("Luck are on your side today. You win a new pair of gloves. They are of fine make and will protect you well.");
             player.addMaxHp(10);
+            System.out.println("Hp: + 10");
         } else if (prize == 1){
             System.out.println("Lady Luck is smiling at you today. You win a new whetting stone. It will come in handy.");
             player.addAttack(3);
+            System.out.println("Attack: + 3");
         } else if (prize == 2) {
             System.out.println("You are blessed with good luck today. You walk away with the ruby amulet. It was the most valuable prize, power glowing in the gem.");
             player.addMaxHp(10);
             player.addAttack(3);
+            System.out.println("Hp: + 10\nAttack: + 3");
         }
 
         System.out.println("When you set out again you realised you ate way too much. You are sure you've gained some weight, you feel slower than before.");
