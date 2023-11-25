d943db88bd10992fc23c04e1772d89add9868e4f
AltheaOlofsson <althea.olofsson@iths.se>
2023-11-25 02:55:06 +0100

    /help

--- InputHandler.java
+++ InputHandler.java
@@ -8,28 +8,31 @@ public class InputHandler {
     }
 
     public String readInput(Player player) {
-        // System.out.print("Enter command: ");
+        GameController gc = new GameController(player);
+
         String input = scanner.nextLine();
 
         if ("/exit".equals(input.toLowerCase())) {
             System.out.println("Exiting the program.");
             System.exit(0);
-        }
-        else if ("/stats".equals(input.toLowerCase())) {
+
+        } else if ("/stats".equals(input.toLowerCase())) {
             displayPlayerStats(player);
+            System.out.println("Press ENTER to close.");
+            scanner.nextLine();
+
+        } else if ("/help".equals(input.toLowerCase())) {
+            gc.clearScreen();
+            System.out.println("\n" + gc.midInstructions + "\n");
             System.out.println("Press ENTER to close.");
             scanner.nextLine();
         }
 
         return input.toLowerCase();
     }
+
     private void displayPlayerStats(Player player) {
         player.displayPlayerStats();
     }
-    // public void close(){
-    //     scanner.close();
-    // }
-
-
 
 }
