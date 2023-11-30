7925bc55a35017a68f48113e40e695316d2f3326
AltheaOlofsson <althea.olofsson@iths.se>
2023-11-30 23:28:22 +0100

    hardly coded event plus endbattle

--- allEvent/HardlyCodedEvent.java
+++ allEvent/HardlyCodedEvent.java
@@ -1,51 +1,44 @@
 
-import java.util.Scanner;
-import java.util.Random;
-
 public class HardlyCodedEvent extends Event {
         Player player;
-        Scanner scanner;
-        private Random random;
         HardlyCodedEvent() {
                 super();
                 this.eventLevel = 20;
         }     
         @Override
         public void execute(Player player, InputHandler InputHandler) {
-                Scanner userInput = new Scanner(System.in);
+                // Scanner userInput = new Scanner(System.in);
                 System.out.println("Press ENTER to continue");
                 System.out.println("At eavening you walk along the path and come across an old man with a cart laden with sacks and boxes. \nThe carts axle is broken and the man looks feeble and distraught.");
-                userInput.nextLine();
+                input.readInput(player);
                 System.out.println("He sees you and his face brightens.");
-                userInput.nextLine();
+                input.readInput(player);
                 System.out.println("- Hoy there young adventurer! Would you mind helping an old man? \nI have a new axle for the cart but my feeble arms and back \nprevents me from lifting the cart to install it.");
-                userInput.nextLine();
+                input.readInput(player);
                 System.out.println("I'll reward you with whatever my hand first touches when I reach inside my cart.");
                 System.out.println("What do you want to do?? \n[1] Help the poor man. \n[2] Rob him blind. \n[3] Leave him on the road. You have important buisness to atend.");
-                Scanner chooseToHelp = new Scanner(System.in);
-                String giveHelpingHand = chooseToHelp.nextLine();
+                String giveHelpingHand = input.readInput(player);
                 for (int wrongInput = 0; wrongInput < 10; wrongInput++) {
                 if (giveHelpingHand.equals("1")) {
                         AdventureGame.clearScreen();
                         System.out.println("Press ENTER to continue");
                         System.out.println("You decide to help him out and together you work to fix the cart.");
-                        userInput.nextLine();
+                        input.readInput(player);
                         System.out.println("It takes a fair bit of time and effort, but you get the work done.");
-                        userInput.nextLine();
+                        input.readInput(player);
                         System.out.println("The man looks at you and smiles. - Thank you young man. I thought I'ld be stuck here all night.");
-                        userInput.nextLine();
+                        input.readInput(player);
                         System.out.println("Well a promise is a promise. He sticks his hand into the pile of sacks.");
-                        userInput.nextLine();
+                        input.readInput(player);
 
-                        Random random = new Random();
-                        int randomLoot = random.nextInt(100) + 1;
+                        int randomLoot = randomgen.nextInt(100) + 1;
                         if (randomLoot == 100) {
                                 System.out.println("He pulls out a beautiful sword and his smile immediately disappears.");
-                                userInput.nextLine();
+                                input.readInput(player);
                                 System.out.println("- Just my luck. First my cart breaks and now I have to give away a legendary sword.");
-                                userInput.nextLine();
+                                input.readInput(player);
                                 System.out.println("But by my honour I'll stand by my word.");
-                                userInput.nextLine();
+                                input.readInput(player);
                                 System.out.println("You've gained: Legendary sword Excalibre");
                                 player.excalibre = 1;
                                 player.addAttack(100);
@@ -54,32 +47,32 @@ public class HardlyCodedEvent extends Event {
                                 player.addCurrentHp(300);
                         } else if (randomLoot <= 99 && randomLoot > 90) {
                                 System.out.println("He pulls out a sleek, curvy shortsword and hands it to you.");
-                                userInput.nextLine();
+                                input.readInput(player);
                                 System.out.println("- Now thats a blade of marvels. It glows blue if there are orcs around.");
-                                userInput.nextLine();
+                                input.readInput(player);
                                 System.out.println("You've gained: Stung.");
                                 player.addAttack(20);
                                 player.addSpeed(20);
                         } else if (randomLoot <= 89 && randomLoot > 70) {
                                 System.out.println("He pulls out a breastplate of peculiar shape. It looks almost like the gaunt chest of a burnt demon.");
-                                userInput.nextLine();
+                                input.readInput(player);
                                 System.out.println("- Ah. This is a very effective armor, created by fusing ebony with the liver of a Deedra.");
-                                userInput.nextLine();
+                                input.readInput(player);
                                 System.out.println("You've gained: Deedric armor.");
                                 player.addMaxHp(20);
                                 player.addSpeed(15);
                         } else if (randomLoot <= 69 && randomLoot > 50) {
                                 System.out.println("He pulls out a branch that holds two golden apples and hands it to you.");
-                                userInput.nextLine();
+                                input.readInput(player);
                                 System.out.println("- Oh these are very interesting. They are said to provide vitality to even those close to death.");
-                                userInput.nextLine();
+                                input.readInput(player);
                                 System.out.println("You remove the apples from the branch and put them in your pouch.");
                                 player.addApple(2);
                                 System.out.println("You have " + player.getApple() + "/4 golden apples.");
-                                userInput.nextLine();
+                                input.readInput(player);
                                 System.out.println("Do you want to keep the branch? \n[1] Yes\n[2] No");
-                                userInput.nextLine();
-                                if (userInput.equals("1") || userInput.equals("Yes") || userInput.equals("yes") || userInput.equals("y")) {
+                                String branch = input.readInput(player);
+                                if (branch.equals("1") || branch.equals("y")) {
                                         System.out.println("You keep the branch. Who knows? It could be usefull.");
                                         player.branch = 1;
                                 } else {
@@ -87,14 +80,14 @@ public class HardlyCodedEvent extends Event {
                                 }
                         } else if (randomLoot <= 49 && randomLoot > 30) {
                                 System.out.println("He pulls out a golden dagger with a ruby fitted into the hilt.");
-                                userInput.nextLine();
+                                input.readInput(player);
                                 System.out.println("- Ooh be careful when using this one. It was found in the ruins of a once amazing city.");
-                                userInput.nextLine();
+                                input.readInput(player);
                                 System.out.println("- It is said that a great evil sleeps there and that everything in the ruins are connected to it.");
-                                userInput.nextLine();
+                                input.readInput(player);
                                 System.out.println("Do you want to take the dagger?\n[1] Yes\n[2] No");
-                                userInput.nextLine();
-                                 if (userInput.equals("1") || userInput.equals("Yes") || userInput.equals("yes") || userInput.equals("y")) {
+                                String dagger = input.readInput(player);
+                                 if (dagger.equals("1") || dagger.equals("yes") || dagger.equals("y")) {
                                         System.out.println("You've gained: Ruby hilted dagger.");
                                         player.decreaseMaxHp(10);
                                         player.decreaseCurrentHp(10);
@@ -104,23 +97,23 @@ public class HardlyCodedEvent extends Event {
                                  }
                         } else if (randomLoot <= 29 && randomLoot > 10) {
                                 System.out.println("He pulls out a helmet that has a faceplate fastened with a long thing spike in the front.");
-                                userInput.nextLine();
+                                input.readInput(player);
                                 System.out.println("- This is an interesting piece. It belongs to an elite fighting group based in the desert city Astaper.");
-                                userInput.nextLine();
+                                input.readInput(player);
                                 System.out.println("- The soldiers are all slave-eunuchs and very effective warriors.");
-                                userInput.nextLine();
+                                input.readInput(player);
                                 System.out.println("You've gained: Helm of the unsallied.");
                                 player.addMaxHp(15);
                                 player.addCurrentHp(15);
                         } else if (randomLoot <= 9 && randomLoot < 1) {
                                 System.out.println("He pulls out a beautiful green cloak and hands it to you.");
-                                userInput.nextLine();
+                                input.readInput(player);
                                 System.out.println("You've gained: Green cloak.");
                         } else {
                                 System.out.println("He pulls out a dirty rag.");
-                                userInput.nextLine();
+                                input.readInput(player);
                                 System.out.println("- Whew seems to be my lucky day.");
-                                userInput.nextLine();
+                                input.readInput(player);
                                 System.out.println("You've gained: Dirty rag.");
                         }
                         player.addExperience(100);
@@ -128,7 +121,7 @@ public class HardlyCodedEvent extends Event {
                 } else if (giveHelpingHand.equals("2")) {
                         AdventureGame.clearScreen();
                         System.out.println("You pull out your weapon and threaten the old man. He gives you his moneypouch while cursing you.");
-                        userInput.nextLine();
+                        input.readInput(player);
                         System.out.println("While walking away you feel yourself loosing strenght.");
                         player.decreaseAttack(3);
                         player.decreaseCurrentHp(5);
@@ -144,7 +137,6 @@ public class HardlyCodedEvent extends Event {
                 }
                 }
                 
-                chooseToHelp.close();
         }
         
 }
