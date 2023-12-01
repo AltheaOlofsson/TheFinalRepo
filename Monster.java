fe423704fc8554604f4da526512e063db8784a63
BiggansFot <81302543+BiggansFot@users.noreply.github.com>
2023-12-01 10:53:21 +0100

    rebalanced monsters

--- Monster.java
+++ Monster.java
@@ -1,20 +1,20 @@
 public enum Monster {
     
-    GOBLIN ("Goblin", 20, 4, 8, 2, 15, 70, false),
-    WOLF("Wolf", 25, 5, 7, 2, 20, 60, false),
-    GNOLL("Gnoll", 20, 2, 8, 2, 20, 70, false),
-    ELF("Elf", 30, 8, 12, 3, 30, 80, false),
-    ORC("Orc", 40, 6, 8, 3, 25, 70, false),
+    GOBLIN ("Goblin", 25, 2, 5, 2, 15, 50, false),
+    WOLF("Wolf", 30, 3, 4, 2, 20, 40, false),
+    GNOLL("Gnoll", 20, 5, 8, 2, 20, 50, false),
+    ELF("Elf", 35, 8, 12, 3, 30, 60, false),
+    ORC("Orc", 50, 6, 7, 3, 25, 50, false),
     DIRE_WOLF("Dire wolf", 35, 8, 10, 3, 40, 60, false),
-    GIANT_SPIDER("Giant spider", 35, 4, 14, 4, 35, 90, false),
-    BEAR("Bear", 55, 8, 12, 4, 30, 80, false),
-    OGRE("Ogre", 55, 14, 16, 6, 30, 70, false),
-    TROLL("Troll", 65, 5, 20, 6, 25, 70, false),
-    WURM("Wurm", 60, 10, 15, 6, 45, 60, false),
-    VAMPIRE("Vampire", 70, 16, 20, 7, 250, 90, true),
-    WEREWOLF("Werewolf", 90, 15, 25, 7, 50, 90, false),
-    DEMON("Demon", 120, 20, 30, 8, 80, 90, false),
-    CHIMERA("Chimera", 130, 20, 35, 9, 90, 90, false);
+    GIANT_SPIDER("Giant spider", 45, 4, 14, 4, 35, 70, false),
+    BEAR("Bear", 55, 8, 12, 4, 30, 70, false),
+    OGRE("Ogre", 75, 14, 16, 6, 40, 50, false),
+    TROLL("Troll", 95, 5, 20, 6, 25, 60, false),
+    WURM("Wurm", 70, 12, 15, 6, 45, 50, false),
+    VAMPIRE("Vampire", 80, 18, 24, 7, 250, 70, true),
+    WEREWOLF("Werewolf", 90, 25, 35, 7, 50, 70, false),
+    DEMON("Demon", 120, 20, 40, 8, 80, 80, false),
+    CHIMERA("Chimera", 150, 20, 45, 9, 90, 90, false);
 
     String name;
     int hp;
